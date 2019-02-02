88999ca4d000ec91d28dbcdedf71bfd9544ad21a
Mujtaba Farooq <mujtabafarooq01@gmail.com>
2019-02-02 16:18:53 +0000

    Project Update: Fix running issue with ios

--- HeartHealth/src/app/app.component.ts
+++ HeartHealth/src/app/app.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { TabsPage } from '../pages/tabs/tabs';
 import { LoginPage } from "../pages/authentication/login/login";
-import { AuthenticationProvider } from "../providers/authentication/authentication";
+
 
 @Component({
   templateUrl: 'app.html'
@@ -13,13 +11,7 @@ import { AuthenticationProvider } from "../providers/authentication/authenticati
 export class MyApp {
   rootPage:any = LoginPage;
 
-  constructor(
-    platform: Platform, 
-    statusBar: StatusBar, 
-    splashScreen: SplashScreen,
-    authenticationProvider: AuthenticationProvider
-    
-    ) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
--- HeartHealth/src/providers/prediction/prediction.ts
+++ HeartHealth/src/providers/prediction/prediction.ts
@@ -22,39 +22,39 @@ export class PredictionProvider {
   }
   films: Observable<any>;
 
-  test() {
-    console.log("Test Called in provider")
-    // http://192.168.0.33:5000/timestamp
+  // test() {
+  //   console.log("Test Called in provider")
+  //   // http://192.168.0.33:5000/timestamp
 
-    //  this.http.get("http://192.168.0.15:5000/timestamp")
-    //  .map(res => res.json()).subscribe(data => {
-    //   console.log(data)
-    // })
+  //   //  this.http.get("http://192.168.0.15:5000/timestamp")
+  //   //  .map(res => res.json()).subscribe(data => {
+  //   //   console.log(data)
+  //   // })
 
-    this.http.get("http://192.168.0.33:5000/timestamp").subscribe(data => {
-      console.log(data);
-    });
-
-
-    this.http.post("http://192.168.0.33:5000/postjson",{"millis":"1548510875488"}).subscribe(data => {
-      console.log(data);
-    });
-    //1548510875488
-
-    // this.films.map(res => res.json()).subscribe(data => {
-    //   console.log(data)
-    // })
+  //   this.http.get("http://192.168.0.33:5000/timestamp").subscribe(data => {
+  //     console.log(data);
+  //   });
 
 
-    // .subscribe(data => {
-    //   console.log(data)
-    // })
+  //   this.http.post("http://192.168.0.33:5000/postjson",{"millis":"1548510875488"}).subscribe(data => {
+  //     console.log(data);
+  //   });
+  //   //1548510875488
 
-    // .map(res => console.log(res.status))
+  //   // this.films.map(res => res.json()).subscribe(data => {
+  //   //   console.log(data)
+  //   // })
 
-    // .subscribe(data => {
-    //   console.log(data)
-    // })
-  }
+
+  //   // .subscribe(data => {
+  //   //   console.log(data)
+  //   // })
+
+  //   // .map(res => console.log(res.status))
+
+  //   // .subscribe(data => {
+  //   //   console.log(data)
+  //   // })
+  // }
 
 }
